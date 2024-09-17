c8ab24d2837aff32a510776549898a5e5f52fe50
Darek Mazur <dev@darekmazur.it>
2024-09-17 19:11:10 +0200

    Prepare global styles

---
+++ src/components/Molecules/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+const Header = () => {
+  return (
+    <header>
+      <Typography variant={'h1'}>Jak dobrze znasz Zamaskowanego Krzyżowca?</Typography>
+    </header>
+  );
+};
+
+export default Header;
