6c37679f7a21cb260d1c5b76c8108227396eb139
Darek Mazur <dev@darekmazur.it>
2024-09-18 04:43:33 +0200

    Move styles to separate file

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,44 +1,5 @@
 import { Box, Button, TextField } from '@mui/material';
-import { theme } from '../../lib/theme.tsx';
-
-const styledForm = {
-  m: '3rem 0',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  position: 'relative'
-};
-
-const styledButton = {
-  fontFamily: '"Russo One", sans-serif;',
-  width: '10rem',
-  height: '3rem',
-  m: '1rem 1.2rem',
-  color: theme.palette.text.secondary,
-  backgroundColor: theme.palette.text.primary,
-  alignSelf: 'self-end'
-};
-
-const styledInput = {
-  backgroundColor: theme.palette.backgroundColorLight?.paper,
-  width: '30rem',
-  border: 'none',
-  fontSize: '1.5rem',
-  mb: '3rem',
-  '&:-webkit-autofill': {
-    WebkitBoxShadow: `0 0 0 100px ${theme.palette.backgroundColorLight?.paper} inset`,
-    WebkitTextFillColor: theme.palette.secondary.light
-  },
-  label: {
-    color: theme.palette.secondary.light,
-    '&.Mui-focused': {
-      color: theme.palette.secondary.light
-    }
-  },
-  '&.Mui-focused': {
-    label: { color: theme.palette.secondary.light }
-  }
-};
+import { styledButton, styledForm, styledInput } from './Form.styles.ts';
 
 const Form = () => {
   return (
