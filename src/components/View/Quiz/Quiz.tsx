d44470a0c5fba672a0571c1683d24a4681bb4641
Darek Mazur <dev@darekmazur.it>
2024-09-19 01:35:39 +0200

    Move quiz questions to seprate component

--- src/components/View/Quiz/Quiz.tsx
+++ src/components/View/Quiz/Quiz.tsx
@@ -1,23 +1,10 @@
-import {
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Container,
-  Dialog,
-  DialogActions,
-  DialogTitle,
-  List,
-  ListItemButton,
-  ListItemText,
-  Typography
-} from '@mui/material';
-import { useContext, useEffect, useState, MouseEvent } from 'react';
+import { Button, Container, Dialog, DialogActions, DialogTitle, Typography } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import { initData, QuizContext } from '../../../lib/AppProvides.tsx';
-import { db } from '../../../lib/data.ts';
 import { useNavigate, useParams } from 'react-router-dom';
 import { styledButton } from '../../Form/Form.styles.ts';
 import { theme } from '../../../lib/theme.tsx';
+import QuizItem from '../../QuizItem/QuizItem.tsx';
 
 const styledQuitButton = {
   ...styledButton,
@@ -25,14 +12,12 @@ const styledQuitButton = {
 };
 
 const Quiz = () => {
-  const { score, player, quizLevel, setResults, setStartTime, setLevel, setQuizPlayer } =
+  const { player, quizLevel, setResults, setStartTime, setLevel, setQuizPlayer } =
     useContext(QuizContext);
   const { quizId } = useParams();
   const navigate = useNavigate();
 
-  const [active, setActive] = useState(true);
   const [currentLevel, setCurrentLevel] = useState<string | undefined>();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [modal, setModal] = useState<boolean>(false);
 
   const getCurrentLevel = () => {
@@ -48,48 +33,6 @@ const Quiz = () => {
         break;
       default:
         break;
-    }
-  };
-
-  const questionsList = db.filter(
-    (question) => question.level?.toLowerCase() === quizId && !question.isBonus
-  );
-
-  const bonusQuestion = db.filter(
-    (question) => question.level?.toLowerCase() === quizId && question.isBonus
-  );
-
-  useEffect(() => {
-    if (score === 10) {
-      setCurrentQuestion(11);
-      setActive(true);
-    }
-  }, [score]);
-
-  const answers: number[] = [];
-
-  do {
-    const random = Math.floor(Math.random() * 4 + 1);
-
-    if (!answers.includes(random)) {
-      answers.push(random);
-    }
-  } while (answers.length < 4);
-
-  const handleChoseAnswer = (e: MouseEvent<HTMLButtonElement>): void => {
-    const event = e.target as HTMLButtonElement;
-    setActive(false);
-
-    if (
-      event.textContent === questionsList[currentQuestion]?.check ||
-      event.textContent === bonusQuestion[0].check
-    ) {
-      setResults(score + 1);
-    }
-
-    if (currentQuestion < 9) {
-      setCurrentQuestion(currentQuestion + 1);
-      setActive(true);
     }
   };
 
@@ -126,50 +69,7 @@ const Quiz = () => {
         Witaj, {player}!
       </Typography>
       <Typography component='p'>Grasz na poziomie {currentLevel ? currentLevel : null}</Typography>
-
-      <Card sx={{ width: 500, my: 2 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={currentQuestion !== 11 ? questionsList[currentQuestion].img : bonusQuestion[0].img}
-          title='quiz question cover'
-        />
-        <CardContent>
-          <Typography gutterBottom variant='h5' component='h3' align='center'>
-            {currentQuestion !== 11
-              ? questionsList[currentQuestion].question.replaceAll('&#39;', "'")
-              : bonusQuestion[0].question.replaceAll('&#39;', "'")}
-          </Typography>
-          <Typography variant='body2' component='div'>
-            <List sx={{ width: '100%' }} component='nav'>
-              {answers.map((option) => (
-                <ListItemButton
-                  component='div'
-                  disabled={!active}
-                  onClick={handleChoseAnswer}
-                  key={
-                    currentQuestion !== 11
-                      ? questionsList[currentQuestion].id + option
-                      : bonusQuestion[0].id + option
-                  }
-                >
-                  <ListItemText
-                    primary={
-                      currentQuestion !== 11
-                        ? questionsList[currentQuestion][
-                            `ans${option}` as 'ans1' | 'ans2' | 'ans3' | 'ans4'
-                          ].replaceAll('&#39;', "'")
-                        : bonusQuestion[0][
-                            `ans${option}` as 'ans1' | 'ans2' | 'ans3' | 'ans4'
-                          ].replaceAll('&#39;', "'")
-                    }
-                  />
-                </ListItemButton>
-              ))}
-            </List>
-          </Typography>
-        </CardContent>
-      </Card>
-
+      <QuizItem />
       <Button sx={styledQuitButton} onClick={handleQuit}>
         Przerwij
       </Button>
