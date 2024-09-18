68b48502226d5c4d3453298c12c2d4884541e669
Darek Mazur <dev@darekmazur.it>
2024-09-18 16:33:24 +0200

    Handle quiz form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,36 @@
 import { Box, Button, TextField } from '@mui/material';
 import { styledButton, styledForm, styledInput } from './Form.styles.ts';
+import { ChangeEvent, MouseEvent, useContext } from 'react';
+import { QuizContext } from '../../lib/AppProvides.tsx';
 
 const Form = () => {
+  const { quizLevel, player, setQuizPlayer, setStartTime } = useContext(QuizContext);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuizPlayer(e.target.value || null);
+  };
+
+  const handleStart = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    setStartTime(Date.now());
+  };
+
   return (
     <Box component='form' sx={styledForm}>
-      <TextField label={'Nick/Imię'} variant='filled' sx={styledInput} />
-      <Button variant='contained' type='submit' sx={styledButton}>
+      <TextField
+        label={'Nick/Imię'}
+        variant='filled'
+        sx={styledInput}
+        onChange={handleInputChange}
+      />
+      <Button
+        disabled={!(player && quizLevel)}
+        variant='contained'
+        type='submit'
+        sx={styledButton}
+        onClick={handleStart}
+      >
         Start
       </Button>
     </Box>
