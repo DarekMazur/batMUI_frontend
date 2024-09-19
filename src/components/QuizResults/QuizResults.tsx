8eb82e88bb3a02046534aca85741728ffcdbfe63
Darek Mazur <dev@darekmazur.it>
2024-09-20 00:50:25 +0200

    Calculate final score

--- src/components/QuizResults/QuizResults.tsx
+++ src/components/QuizResults/QuizResults.tsx
@@ -1,24 +1,33 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { useResults } from '../../lib/hooks/useResults.tsx';
-import { calculateRank } from '../../lib/helpers.ts';
+import { calculateFinalScore, calculateRank } from '../../lib/helpers.ts';
 import { useContext } from 'react';
 import { QuizContext } from '../../lib/AppProvides.tsx';
 
 const QuizResults = () => {
-  const { score } = useContext(QuizContext);
+  const { score, start, end, quizLevel } = useContext(QuizContext);
   const playerRank = useResults(calculateRank(score));
+
+  let timeMS = end - start;
+
+  if (timeMS > 10 * 60000) {
+    timeMS = 10 * 60000;
+  }
+
+  const finalScore = calculateFinalScore(score, timeMS, quizLevel as 'easy' | 'normal' | 'hard');
 
   return (
     <Card sx={{ width: 500, my: 2 }}>
       <CardMedia
-        sx={{ height: 300 }}
+        sx={{ height: 500 }}
         image={playerRank.rankImage as string}
         title='quiz results cover'
       />
       <CardContent>
         <Typography gutterBottom variant='h5' component='h3' align='center'>
-          {playerRank.rankName}
+          Zdobywasz {finalScore} punktów ({Math.round((score / 10) * 100)}%).
         </Typography>
+        <Typography variant='body2' component='div'></Typography>
         <Typography variant='body2' component='div'>
           {playerRank.rankDescription}
         </Typography>
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,3 +3,32 @@ import { MAX_COUNT } from './vars.ts';
 export const calculateRank = (score: number) => {
   return score / MAX_COUNT;
 };
+
+export const calculateFinalScore = (
+  score: number,
+  timeMS: number,
+  level: 'easy' | 'normal' | 'hard'
+) => {
+  const timePerQuestion = ((10 - (score > 10 ? 10 : score)) * 60000 + timeMS) / 10;
+
+  let levelParam = 1;
+
+  switch (level) {
+    case 'easy':
+      levelParam = 0.8;
+      break;
+    case 'normal':
+      levelParam = 1;
+      break;
+    case 'hard':
+      levelParam = 1.5;
+      break;
+    default:
+      levelParam = 1;
+      break;
+  }
+
+  const scoreRaw = Math.log(timePerQuestion * levelParam);
+
+  return Math.round(scoreRaw * score);
+};
