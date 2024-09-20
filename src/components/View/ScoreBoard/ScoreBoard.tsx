5833858574b4dcc71d66fd7370ecbfd2c7fc8b34
Darek Mazur <dev@darekmazur.it>
2024-09-20 02:21:20 +0200

    Style table

--- src/components/View/ScoreBoard/ScoreBoard.tsx
+++ src/components/View/ScoreBoard/ScoreBoard.tsx
@@ -1,4 +1,5 @@
 import {
+  Container,
   Paper,
   styled,
   Table,
@@ -13,8 +14,8 @@ import { generatePlayers } from '../../../lib/data.ts';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14
@@ -23,7 +24,8 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover
+    backgroundColor: theme.palette.action.hover,
+    color: theme.palette.common.white
   },
   '&:last-child td, &:last-child th': {
     border: 0
@@ -31,35 +33,50 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 const ScoreBoard = () => {
-  const players = generatePlayers();
+  const players = generatePlayers().sort((a, b) => b.score - a.score);
+
+  const translatedLevel = (level: string | null) => {
+    switch (level) {
+      case 'Easy':
+        return 'Łatwy';
+      case 'Normal':
+        return 'Normalny';
+      case 'Hard':
+        return 'Trudny';
+      default:
+        return null;
+    }
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label='customized table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Pozycja</StyledTableCell>
-            <StyledTableCell align='right'>Gracz</StyledTableCell>
-            <StyledTableCell align='right'>Wynik</StyledTableCell>
-            <StyledTableCell align='right'>Czas</StyledTableCell>
-            <StyledTableCell align='right'>Poziom</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {players.map((player, index) => (
-            <StyledTableRow key={player.id}>
-              <StyledTableCell component='th' scope='row'>
-                {index + 1}
-              </StyledTableCell>
-              <StyledTableCell align='right'>{player.username}</StyledTableCell>
-              <StyledTableCell align='right'>{player.score}</StyledTableCell>
-              <StyledTableCell align='right'>{player.time}</StyledTableCell>
-              <StyledTableCell align='right'>{player.level}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Container sx={{ my: 3 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Pozycja</StyledTableCell>
+              <StyledTableCell align='right'>Gracz</StyledTableCell>
+              <StyledTableCell align='right'>Wynik</StyledTableCell>
+              <StyledTableCell align='right'>Czas</StyledTableCell>
+              <StyledTableCell align='right'>Poziom</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {players.map((player, index) => (
+              <StyledTableRow key={player.id}>
+                <StyledTableCell component='th' scope='row'>
+                  {index + 1}
+                </StyledTableCell>
+                <StyledTableCell align='right'>{player.username}</StyledTableCell>
+                <StyledTableCell align='right'>{player.score}</StyledTableCell>
+                <StyledTableCell align='right'>{player.time}</StyledTableCell>
+                <StyledTableCell align='right'>{translatedLevel(player.level)}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
