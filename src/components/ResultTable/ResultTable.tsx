f62075876c1b29a4c4346db7270ae3667e6d1e9a
Darek Mazur <dev@darekmazur.it>
2024-09-20 03:50:01 +0200

    Add table pagination

--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -1,5 +1,13 @@
-import { Paper, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
-import { FC } from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow
+} from '@mui/material';
+import { ChangeEvent, FC, useMemo, useState } from 'react';
 import { StyledTableCell, StyledTableRow } from './ResultTable.style.ts';
 
 interface IPlayerProps {
@@ -11,6 +19,9 @@ interface IPlayerProps {
 }
 
 const ResultTable: FC<{ playersList: IPlayerProps[] }> = ({ playersList }) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
   const translatedLevel = (level: string | null) => {
     switch (level) {
       case 'Easy':
@@ -24,33 +35,59 @@ const ResultTable: FC<{ playersList: IPlayerProps[] }> = ({ playersList }) => {
     }
   };
 
+  const visiblePlayers = useMemo(
+    () => [...playersList].slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [page, rowsPerPage]
+  );
+
+  const handleChangePage = (_e: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (e: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(e.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label='customized table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Pozycja</StyledTableCell>
-            <StyledTableCell align='right'>Gracz</StyledTableCell>
-            <StyledTableCell align='right'>Wynik</StyledTableCell>
-            <StyledTableCell align='right'>Czas</StyledTableCell>
-            <StyledTableCell align='right'>Poziom</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {playersList.map((player, index) => (
-            <StyledTableRow key={player.id}>
-              <StyledTableCell component='th' scope='row'>
-                {index + 1}
-              </StyledTableCell>
-              <StyledTableCell align='right'>{player.username}</StyledTableCell>
-              <StyledTableCell align='right'>{player.score}</StyledTableCell>
-              <StyledTableCell align='right'>{player.time}</StyledTableCell>
-              <StyledTableCell align='right'>{translatedLevel(player.level)}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Pozycja</StyledTableCell>
+              <StyledTableCell align='right'>Gracz</StyledTableCell>
+              <StyledTableCell align='right'>Wynik</StyledTableCell>
+              <StyledTableCell align='right'>Czas</StyledTableCell>
+              <StyledTableCell align='right'>Poziom</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {visiblePlayers.map((player, index) => (
+              <StyledTableRow key={player.id}>
+                <StyledTableCell component='th' scope='row'>
+                  {index + 1}
+                </StyledTableCell>
+                <StyledTableCell align='right'>{player.username}</StyledTableCell>
+                <StyledTableCell align='right'>{player.score}</StyledTableCell>
+                <StyledTableCell align='right'>{player.time}</StyledTableCell>
+                <StyledTableCell align='right'>{translatedLevel(player.level)}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component='div'
+        count={playersList.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        labelRowsPerPage='Wyników na stronę'
+      />
+    </>
   );
 };
 
