5400f3638d71c5b0970d96a34b4cd17c56921854
Darek Mazur <dev@darekmazur.it>
2024-09-20 02:47:25 +0200

    Add tabs with results by level

--- src/components/View/ScoreBoard/ScoreBoard.tsx
+++ src/components/View/ScoreBoard/ScoreBoard.tsx
@@ -1,13 +1,56 @@
-import { Container } from '@mui/material';
+import { Box, Container, Tab, Tabs } from '@mui/material';
 import { generatePlayers } from '../../../lib/data.ts';
 import ResultTable from '../../ResultTable/ResultTable.tsx';
+import { ReactNode, SyntheticEvent, useState } from 'react';
+
+interface TabPanelProps {
+  children?: ReactNode;
+  index: number;
+  value: number;
+}
 
 const ScoreBoard = () => {
   const players = generatePlayers().sort((a, b) => b.score - a.score);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_e: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div role='tabpanel' hidden={value !== index} {...other}>
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
 
   return (
     <Container sx={{ my: 5 }} maxWidth='md'>
-      <ResultTable playersList={players} />
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label='basic tabs example'>
+            <Tab label='Open' />
+            <Tab label='Łatwy' />
+            <Tab label='Normalny' />
+            <Tab label='Trudny' />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <ResultTable playersList={players} />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <ResultTable playersList={players.filter((player) => player.level === 'Easy')} />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          <ResultTable playersList={players.filter((player) => player.level === 'Normal')} />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={3}>
+          <ResultTable playersList={players.filter((player) => player.level === 'Hard')} />
+        </CustomTabPanel>
+      </Box>
     </Container>
   );
 };
